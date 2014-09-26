ded2f5c49f2d9260bd1bdc0dc3e309dfb3d04220
JoeCarty <jcarty@stcharlessd.org>
2014-09-26 13:19:08 -0500

    Fixed Program

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -10,7 +10,7 @@ impart random
 die1 = random.randint(1, 6) 
 die2 = random.randrange(6)
 
-total = 'die1' + 'die2'
+total = die1 + die2
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
 
